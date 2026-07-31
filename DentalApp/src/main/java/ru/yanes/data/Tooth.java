33927f6d3d7c8d3c11be9868861849c97e651a4d
Ryabikove <lodogatze@mail.ru>
2026-07-31 15:46:04 +0300

    Make 8 teeth unavailable by default - solve #10

--- DentalApp/src/main/java/ru/yanes/data/Tooth.java
+++ DentalApp/src/main/java/ru/yanes/data/Tooth.java
@@ -4,6 +4,14 @@ import lombok.Data;
 
 @Data
 public class Tooth {
-	private boolean available = true;
 	private final int position;
+	private boolean available;
+
+	Tooth(int position) {
+		this.position = position;
+		this.available = switch (this.position) {
+			case 48, 28, 38, 18 -> false;
+			default -> true;
+		};
+	}
 }
