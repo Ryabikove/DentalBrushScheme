378be9c63c56df381ce9603ad374868c9cff7f65
Ilya <164762572+Ryabikove@users.noreply.github.com>
2026-08-04 15:21:59 +0300

    Revert "3 add interaction with spaces"

--- DentalApp/src/main/java/ru/yanes/data/Mouth.java
+++ DentalApp/src/main/java/ru/yanes/data/Mouth.java
@@ -8,31 +8,18 @@ public class Mouth {
 	private final Tooth[] teeth;
 	private final Space[] spaces;
 
-	public Mouth() {
-		int[] TEETH_POSITION = new int[] {28, 27, 26, 25, 24, 23, 22, 21, 11, 12, 13, 14, 15, 16, 17, 18, 48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38};
+	public Mouth(int teeth) {
+		this.teeth = new Tooth[teeth];
+		this.spaces = new Space[teeth - 1];
 
-		int teethNumber = TEETH_POSITION.length;
-		this.teeth = new Tooth[teethNumber];
-
-		for(int i = 0; i < teethNumber; ++i) {
-			this.teeth[i] = new Tooth(TEETH_POSITION[i]);
-//			System.out.println("Tooth - " + i + ", with position - " + TEETH[i].getPosition() + ";");
+		for(int i = 0; i < teeth; ++i) {
+			this.teeth[i] = new Tooth(i % 8 + 1 + 10 * (i / 8 + 1));
 		}
 
-		// Number of spaces for each jaw lesser by 1 (for 2 jaws by 2)
-		int spaceNumber = teeth.length - 2;
-		this.spaces = new Space[spaceNumber];
-
-
-		for(int i = 0; i < spaceNumber; ++i) {
-			//As we don't have spaces between 38 and 28 teeth we skip this pare
-			if (i >= 15) {
-				this.spaces[i] = new Space(this.teeth[i + 1], this.teeth[i + 2], i);
-			} else {
-				this.spaces[i] = new Space(this.teeth[i], this.teeth[i + 1], i);
-			}
-//			System.out.println("Space - " + i + ", with left tooth - " + this.SPACES[i].getLeftTooth().getPosition() + ", and right tooth - " + this.SPACES[i].getRightTooth().getPosition() + ";");
+		for(int i = 0; i < teeth - 1; ++i) {
+			this.spaces[i] = new Space(this.teeth[i], this.teeth[i + 1]);
 		}
+
 	}
 
 	public Tooth getTooth(int pos) {
